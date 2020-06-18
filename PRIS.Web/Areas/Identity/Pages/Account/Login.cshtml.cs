4fd73b0923002d81905999f83337d9054da794b8
Denis Zamaro <denis.zamaro@gmail.com>
2020-06-18 14:10:33 +0300

    Login UI + Welcome page

--- PRIS.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PRIS.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace PRIS.Web.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Home/Index");
 
             if (ModelState.IsValid)
             {
