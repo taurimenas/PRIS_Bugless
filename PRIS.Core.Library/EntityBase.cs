c68203a68a81cb432270d4358d8346c4a3b308e9
taurimenas <taurimenas01@gmail.com>
2020-06-20 01:47:19 +0300

    Added EntityBase

---
+++ PRIS.Core.Library/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRIS.Core.Library
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+    }
+}
