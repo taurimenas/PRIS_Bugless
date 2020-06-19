bb015e59a0e8ee31dee518f7a098dcaf84f0a992
taurimenas <taurimenas01@gmail.com>
2020-06-20 01:44:28 +0300

    Created project library

---
+++ PRIS.Core.Library/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRIS.Core.Library
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
