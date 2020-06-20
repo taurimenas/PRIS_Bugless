9d4d20166b13b2da5919431abc4e1fe5bdf97527
taurimenas <taurimenas01@gmail.com>
2020-06-20 11:30:25 +0300

    Final db configuration

--- PRIS.Web/Data/ApplicationDbContext.cs
+++ PRIS.Web/Data/ApplicationDbContext.cs
@@ -18,9 +18,20 @@ namespace PRIS.Web.Data
         public DbSet<City> Cities { get; set; }
         public DbSet<ConversationResult> ConversationResults { get; set; }
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Models.Program> Programs { get; set; }
+        public DbSet<Program> Programs { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<StudentsCourse> StudentsCourses { get; set; }
         public DbSet<TestResult> TestResults { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().ToTable("City");
+            modelBuilder.Entity<ConversationResult>().ToTable("ConversationResult");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Program>().ToTable("Program");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<StudentsCourse>().ToTable("StudentsCourse");
+            modelBuilder.Entity<TestResult>().ToTable("TestResult");
+        }
     }
 }
--- PRIS.Web/Startup.cs
+++ PRIS.Web/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
 
 namespace PRIS.Web
 {
