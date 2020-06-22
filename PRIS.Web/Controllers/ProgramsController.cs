97b8944b11346614a800947fff08f1e86bd587e1
Aurimas Rinkevicius <aurimui.rinkeviciui@gmail.com>
2020-06-22 22:12:20 +0300

    Created programs list

---
+++ PRIS.Web/Controllers/ProgramsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PRIS.Web.Data;
+
+namespace PRIS.Web.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProgramsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Programs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Programs.ToListAsync());
+        }
+
+        // GET: Programs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var program = await _context.Programs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (program == null)
+            {
+                return NotFound();
+            }
+
+            return View(program);
+        }
+
+        // GET: Programs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Programs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Id")] Models.Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(program);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(program);
+        }
+
+        // GET: Programs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var program = await _context.Programs.FindAsync(id);
+            if (program == null)
+            {
+                return NotFound();
+            }
+            return View(program);
+        }
+
+        // POST: Programs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Id,Created")] Models.Program program)
+        {
+            if (id != program.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(program);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgramExists(program.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(program);
+        }
+
+        // GET: Programs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var program = await _context.Programs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (program == null)
+            {
+                return NotFound();
+            }
+
+            return View(program);
+        }
+
+        // POST: Programs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var program = await _context.Programs.FindAsync(id);
+            _context.Programs.Remove(program);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProgramExists(int id)
+        {
+            return _context.Programs.Any(e => e.Id == id);
+        }
+    }
+}
