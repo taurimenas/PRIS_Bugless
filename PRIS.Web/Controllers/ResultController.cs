f1a80bbe1c09c10179538270e23e3196d5bc62f3
Sigita Stanynaitė <si.stanynaite@outlook.com>
2020-06-26 09:09:46 +0300

    Created (CRUD) views for Result

---
+++ PRIS.Web/Controllers/ResultController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
+using PRIS.Core.Library.Entities;
+using PRIS.Web.Data;
+using PRIS.Web.Mappings;
+using PRIS.Web.Models;
+
+namespace PRIS.Web.Controllers
+{
+    public class ResultController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResultController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET:Results
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var result = await _context.Results.ToListAsync();
+            var student = await _context.Students.ToListAsync();
+
+            //var studentsResults = await (from s in _context.Students
+            //                     join r in _context.Results on
+            //                     s.Result.Id equals r.Id
+            //                     select new ResultViewModel
+            //                     {
+            //                     FirstName = s.FirstName,
+            //                      LastName = s.LastName,
+            //                      Email = s.Email,
+            //                      PhoneNumber = s.PhoneNumber,
+            //                      Id = r.Id
+            //                     }).ToListAsync();
+
+            List<ResultViewModel> resultViewModels = new List<ResultViewModel>();
+            foreach (var item in result)
+            {
+                resultViewModels.Add(ResultMappings.ToResultViewModel(item));
+                foreach (var resultViewModel in resultViewModels)
+                {
+                    resultViewModel.FinalPoints = resultViewModel.Task1_1 + resultViewModel.Task1_2 + resultViewModel.Task1_3 + resultViewModel.Task2_1 + resultViewModel.Task2_2 + resultViewModel.Task2_3 + resultViewModel.Task3_1 + resultViewModel.Task3_2 + resultViewModel.Task3_3 + resultViewModel.Task3_4;
+                }
+            }
+            foreach (var item in student)
+            {
+                resultViewModels.Add(ResultMappings.ToResultViewModel(item));
+            }
+
+
+            return View(resultViewModels);
+
+        }
+        // GET: Result/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var result = await _context.Results
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+
+            var model = ResultMappings.ToResultViewModel(result);
+            return View(model);
+        }
+
+        //GET: Result/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Result/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Task1_1,Task1_2,Task1_3,Task2_1,Task2_2,Task2_3,Task3_1,Task3_2,Task3_3,Task3_4,Comment")] ResultViewModel resultViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = ResultMappings.ToResultEntity(resultViewModel);
+                _context.Add(result);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resultViewModel);
+        }
+        //GET: Result/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var result = await _context.Results.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(ResultMappings.ToResultViewModel(result));
+        }
+        //POST:Result/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Task1_1,Task1_2,Task1_3,Task2_1,Task2_2,Task2_3,Task3_1,Task3_2,Task3_3,Task3_4,Comment")] ResultViewModel resultViewModel)
+        {
+            var result = ResultMappings.ToResultEntity(resultViewModel);
+
+            if (id != result.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(result);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResultExists(result.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(result);
+        }
+        // GET: Result/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var result = await _context.Results
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(ResultMappings.ToResultViewModel(result));
+        }
+        // POST: Result/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var result = await _context.Results.FindAsync(id);
+            _context.Results.Remove(result);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool ResultExists(int id)
+        {
+            return _context.Results.Any(e => e.Id == id);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PRIS.Web/Models/ResultViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIS.Web.Models
+{
+    public class ResultViewModel
+    {
+
+        public int Id { get; set; }
+        public double Task1_1 { get; set; }
+        public double Task1_2 { get; set; }
+        public double Task1_3 { get; set; }
+        public double Task2_1 { get; set; }
+        public double Task2_2 { get; set; }
+        public double Task2_3 { get; set; }
+        public double Task3_1 { get; set; }
+        public double Task3_2 { get; set; }
+        public double Task3_3 { get; set; }
+        public double Task3_4 { get; set; }
+        public double FinalPoints { get; set; }
+        public string Comment { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
