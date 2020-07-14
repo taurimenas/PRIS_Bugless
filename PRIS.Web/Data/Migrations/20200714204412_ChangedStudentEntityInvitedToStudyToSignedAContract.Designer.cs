67410bbff3ae354bd2e3247cdf2a4c978b3bb1e2
Aurimas <aurimui.rinkeviciui@gmail.com>
2020-07-15 00:02:09 +0300

    Created migration > Changed studentEntity property InvitedToStudy to SignedAContract

--- PRIS.Web/Data/Migrations/20200714204412_ChangedStudentEntityInvitedToStudyToSignedAContract.Designer.cs
+++ PRIS.Web/Data/Migrations/20200714204412_ChangedStudentEntityInvitedToStudyToSignedAContract.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PRIS.Web.Data;
 
 namespace PRIS.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200714204412_ChangedStudentEntityInvitedToStudyToSignedAContract")]
+    partial class ChangedStudentEntityInvitedToStudyToSignedAContract
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PRIS.Web/Data/Migrations/20200714204412_ChangedStudentEntityInvitedToStudyToSignedAContract.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PRIS.Web.Data.Migrations
+{
+    public partial class ChangedStudentEntityInvitedToStudyToSignedAContract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitedToStudy",
+                table: "Student");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SignedAContract",
+                table: "Student",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SignedAContract",
+                table: "Student");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InvitedToStudy",
+                table: "Student",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
