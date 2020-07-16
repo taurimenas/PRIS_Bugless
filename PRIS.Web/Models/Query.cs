18005e194caa6249cc4bd9df037bf821242f3a7a
taurimenas <taurimenas01@gmail.com>
2020-07-16 20:56:22 +0300

    United filtering

---
+++ PRIS.Web/Models/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIS.Web.Models
+{
+    public class Query
+    {
+        public double? MinGrade { get; set; } = 1.5;
+        public double? MaxGrade { get; set; } = 10;
+        public int? ExamId { get; set; }
+        public int? CityId { get; set; }
+        public int? CourseId { get; set; }
+    }
+}
