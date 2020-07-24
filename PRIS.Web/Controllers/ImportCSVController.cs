90f839e0c274527fe820f1098bcbac79e5c55907
Sigita Stanynaitė <si.stanynaite@outlook.com>
2020-07-24 11:46:21 +0300

    Created ImportStudentsDataModel

--- PRIS.Web/Controllers/ImportCSVController.cs
+++ PRIS.Web/Controllers/ImportCSVController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using CsvHelper;
 using System.Text.RegularExpressions;
+using System.Globalization;
+using PRIS.Web.Models;
 
 namespace PRIS.Web.Controllers
 {
@@ -29,25 +31,76 @@ namespace PRIS.Web.Controllers
         public IActionResult ImportCSV(IFormFile file)
         {
             var dir = _environment.ContentRootPath;
-            using (var fileStream = new FileStream(Path.Combine(dir,"file.csv"), FileMode.Create, FileAccess.Write))
+            using (var fileStream = new FileStream(Path.Combine(dir, "file.csv"), FileMode.Create, FileAccess.Write))
             {
                 file.CopyTo(fileStream);
             }
+                    var dt = new DataTable();
             using (StreamReader streamReader = new StreamReader("file.csv"))
             {
+# region for CsvHelper
+
+                //using (var csv = new CsvReader(streamReader, CultureInfo.CurrentCulture))
+                //{
+                //using (var dr = new CsvDataReader(csv))
+                //{
+                //csv.ReadHeader();
+                //dt.Columns.Add("CsvId", typeof(string));
+                //dt.Columns.Add("NameSurname", typeof(string));
+                //dt.Columns.Add("Priority", typeof(string));
+                //dt.Columns.Add("Task1", typeof(string));
+                //dt.Columns.Add("Task2", typeof(string));
+                //dt.Columns.Add("Task3", typeof(string));
+                //dt.Columns.Add("Task4", typeof(string));
+                //dt.Columns.Add("Task5", typeof(string));
+                //dt.Columns.Add("Task6", typeof(string));
+                //dt.Columns.Add("Task7", typeof(string));
+                //dt.Columns.Add("Task8", typeof(string));
+                //dt.Columns.Add("Task9", typeof(string));
+                //dt.Columns.Add("Task10", typeof(string));
+                //dt.Columns.Add("Gender", typeof(string));
+                //dt.Columns.Add("PassedExam", typeof(string));
+                //dt.Columns.Add("ConversationResult", typeof(string));
+                //dt.Columns.Add("InvitationToStudy", typeof(string));
+                //dt.Columns.Add("SignedAContract", typeof(string));
+                //while (csv.Read())
+                //{
+                //    DataRow row = dt.NewRow();
+                //    foreach (DataColumn column in dt.Columns)
+                //    {
+                //        row[column.ColumnName]= csv.GetField(column.DataType, column.ColumnName);
+
+                //    }
+                //    dt.Rows.Add(row);
+                //}
+                // }
+
+
+                //var reco = csv.GetRecords<ImportStudentsData>();
+                //foreach (var item in reco)
+                //{
+                //Console.WriteLine(item);
+
+                //}
+#endregion
                 string studentDataFromCSV;
                 while ((studentDataFromCSV = streamReader.ReadLine()) != null)
                 {
                     Console.WriteLine(studentDataFromCSV);
-                    
-                    var result = studentDataFromCSV.Split(";");
 
-                    foreach (var item in result)
+                    var seperatedData = studentDataFromCSV.Split(";");
+                    
+                    string[] Tasks = new string[10];
+                    for (int i = 4; i < 15 ; i++)
                     {
-                        Console.WriteLine(item);
+                        Tasks[i - 4] = seperatedData[i];
+                    }
+                    foreach (var oneColumnData in seperatedData)
+                    {
+                        Console.WriteLine(oneColumnData);
                     }
                 }
-               
+
             };
 
             return RedirectToAction("Index");
---
+++ PRIS.Web/Models/ImportStudentsData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIS.Web.Models
+{
+    public class ImportStudentsData
+    { 
+        public string CsvId { get; set; }
+        public string NameSurname { get; set; }
+        public string Priority { get; set; }
+        public string Tasks { get; set; }
+        //public string Task1 { get; set; }
+        //public string Task2 { get; set; }
+        //public string Task3 { get; set; }
+        //public string Task4 { get; set; }
+        //public string Task5 { get; set; }
+        //public string Task6 { get; set; }
+        //public string Task7 { get; set; }
+        //public string Task8 { get; set; }
+        //public string Task9 { get; set; }
+        //public string Task10 { get; set; }
+        public string Gender { get; set; }
+        public string PassedExam { get; set; }
+        public string ConversationResult { get; set; }
+        public string InvitationToStudy { get; set; }
+        public string SignedAContract { get; set; }
+        
+
+    }
+}
